2664485cc3fbcfdd31cf14a615117802e5b9dc18
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-14 18:22:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,6 @@ def adding_matz
 
 
 	programmer_hash =
-	:yukihiro_matsumoto => {
-				 known_for: "Ruby",
-				 languages: ["LISP", "C"]
-			 },
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -92,7 +88,7 @@ def adding_matz
 						languages: ["LISP", "C"]
 			 		 }
 
-
+ programmer_hash
 end
 
 def changing_alan
